936ad0f1eb242e834ecaae10c877400d56876832
Ljuben Angelkoski <Ljuben.Angelkoski@netcetera.com>
2021-07-14 18:42:13 +0200

    Initial Commit

---
+++ RecipeFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeFinder
+//
+//  Created by Ljuben Angelkoski on 14.7.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeFinder/RecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderApp.swift
+//  RecipeFinder
+//
+//  Created by Ljuben Angelkoski on 14.7.21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
